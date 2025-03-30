c97e0fc57688e41ba3b4a2fd9546abb7e1548ee6
robertoriy <holdenxhume@gmail.com>
2025-03-30 20:47:15 +0700

    base_api | добавил утилитарный класс

---
+++ src/main/java/ru/edu/nsu/ner/bioes/translator/utils/BioesUtility.java
@@ -0,0 +1,9 @@
+package ru.edu.nsu.ner.bioes.translator.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BioesUtility {
+    public static final String DEFAULT_BIOES_SEPARATOR = "-";
+    public static final String EMPTY_BIOES_SEPARATOR = "";
+}
